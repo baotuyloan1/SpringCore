d6c8289745b55a5a78112d9a13d403861338e71c
unknown <nguyenducbaokey@gmail.com>
2023-04-12 22:35:51 +0700

    Spring With Hibernate - DAO

--- StudentManagement/src/main/java/edu/fa/StudentManagement.java
+++ StudentManagement/src/main/java/edu/fa/StudentManagement.java
@@ -3,6 +3,7 @@ package edu.fa;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import edu.fa.dao.StudentHibernateDao;
 import edu.fa.dao.StudentJdbcDao;
 import edu.fa.dao.StudentJdbcDaoSupport;
 import edu.fa.dao.StudentJdbcTemplateDao;
@@ -11,13 +12,15 @@ import edu.fa.model.Student;
 public class StudentManagement {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-		StudentJdbcDaoSupport dao = context.getBean("studentJdbcDaoSupportClass1", StudentJdbcDaoSupport.class);
-		dao.deleteStudent();
-		dao.insertStudent(new Student(1, "Clover", "Viet Nam"));
-		dao.insertStudent(new Student(2, "Clover 2", "Viet Nam 2"));
-		System.out.println(dao.getAllStudents());
-		System.out.println(dao.countStudents());
-//		System.out.println(dao.nameStudents());
-		System.out.println(dao.getStudentById(1));
+		StudentHibernateDao dao = context.getBean("studentHibernateDao", StudentHibernateDao.class);
+//		dao.deleteStudent();
+//		dao.insertStudent(new Student(1, "Clover", "Viet Nam"));
+//		dao.insertStudent(new Student(2, "Clover 2", "Viet Nam 2"));
+//		System.out.println(dao.getAllStudents());
+//		System.out.println(dao.countStudents());
+////		System.out.println(dao.nameStudents());
+//		System.out.println(dao.getStudentById(1));
+		
+		dao.save(new Student("BaoKey 3","Da Nang"));
 	}
 }
