0d438ae020e4b0383d69c768c3f4d7ac13694bfc
unknown <nguyenducbaokey@gmail.com>
2023-04-12 09:35:20 +0700

    JDBC Connection SQL Server 2019

--- StudentManagement/src/main/java/edu/fa/StudentManagement.java
+++ StudentManagement/src/main/java/edu/fa/StudentManagement.java
@@ -9,6 +9,6 @@ public class StudentManagement {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 		StudentJdbcDao dao = context.getBean("studentJdbcDao", StudentJdbcDao.class);
-		System.out.println("Hello");
+		dao.createConnection();
 	}
 }
--- StudentManagement/src/main/java/edu/fa/dao/StudentJdbcDao.java
+++ StudentManagement/src/main/java/edu/fa/dao/StudentJdbcDao.java
@@ -1,8 +1,42 @@
 package edu.fa.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.stereotype.Component;
 
-@Component
+@Component()
 public class StudentJdbcDao {
+	private String jdbcUrl = "jdbc:sqlserver://DESKTOP-HKVP723\\BAO:1433;databaseName=education;trustServerCertificate=true;";
 
+//	jdbc Connection
+	private Connection connection = null;
+	private Statement statement = null;
+
+	public void createConnection() {
+		if (connection == null) {
+			try {
+				Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "abcd1234");
+				System.out.println("Kết nối thành công đến SQL Server 2019");
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+	}
+
+	private void shutdown() {
+		try {
+			if (statement != null) {
+				statement.close();
+			}
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+	}
 }
